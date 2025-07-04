6bac683020af4580395488024c1642d346ed36c8
muhammed bilal <muhammedbilal6211@gmail.com>
2025-07-04 16:50:14 +0530

    routing basics

--- Backend/server.js
+++ Backend/server.js
@@ -4,4 +4,14 @@ const app = express();
 
 let port = 3000;
 
+// app.httpMethod('url' , handler)
+
+app.get("/", (req, res) => {
+  res.send("haiiii");
+});
+
+app.get("/users", (req, res) => {
+  res.send("users");
+});
+
 app.listen(port, () => console.log("server started"));
